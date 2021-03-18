e302a49c39a167f91308e4e1130822a48eb93333
Evana Islam <evana@clipsalsolar.com>
2021-03-19 09:19:12 +1100

    Update Fetch.Failure.Error model

--- WeatherForecast/Fetch/Fetch.swift
+++ WeatherForecast/Fetch/Fetch.swift
@@ -23,6 +23,25 @@ extension Fetch {
         struct Error: Swift.Error, Equatable, Decodable {
             let title: String?
             let message: String?
+            
+            enum CodingKeys: String, CodingKey {
+                case title
+                case message
+            }
+            
+            init(from decoder: Decoder) throws {
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                self.title = try container.decodeIfPresent(String.self, forKey: .title)
+                self.message = try container.decodeIfPresent(String.self, forKey: .message)
+            }
+            
+            init(
+                title: String? = nil,
+                message: String
+            ) {
+                self.title = title
+                self.message = message
+            }
         }
     }
 }
